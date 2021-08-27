611191f46e50c778029f4ac8e0d3330ac4a0f5d4
Gabriel Ferrante <gabrielsferrante@hotmail.com>
2021-08-26 22:02:22 -0300

    Add Kmeans e KNN

--- Agrupamento/Particional/kmeans.py
+++ Agrupamento/Particional/kmeans.py
@@ -5,3 +5,162 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from matplotlib import pyplot as plt
 
+
+#LENDO O CSV
+
+dados1 = pd.read_csv('kmeans_exemplo1.csv')
+print(dados1)
+
+#Plotando usando a função de espalhamento ("Scatter")
+
+dados1.plot(kind='scatter',x=0,y=1)
+
+"""
+Executando o k-means com a seguinte configuração:
+
+n_clusters=5 (número de clusters)
+n_init=10 (quantidade de inicializações)
+init='random' (inicialização aleatória dos centroides)
+max_iter=300 (maximo de iterações)
+
+"""
+
+kmeans = KMeans(n_clusters=5,n_init=10,init='random',max_iter=300)
+kmeans.fit(dados1)
+
+"""
+Verificando os clusters obtidos
+"""
+
+dados1['cluster'] = kmeans.labels_
+print(dados1)
+
+#Plotando usanfo a função de espalhamento
+sns.scatterplot(data=dados1, x='0', y='1', hue="cluster")
+
+#Centroides
+
+centroids = kmeans.cluster_centers_
+print(centroids)
+
+#Erro quadrático, o que tiver menor valor, é o melhor agrupamento dado
+#varias iterações
+E = kmeans.inertia_
+print(E)
+
+"""
+Vamos testar uma execução do k-means com finalização prematura, ou seja, o algoritmo finaliza antes de convergir.
+"""
+dados1 = pd.read_csv('kmeans_exemplo1.csv')
+
+kmeans = KMeans(n_clusters=5,n_init=1,init='random',max_iter=2)
+kmeans.fit(dados1)
+
+dados1['cluster'] = kmeans.labels_
+
+sns.scatterplot(data=dados1, x='0', y='1', hue="cluster")
+
+
+"""
+Exemplo 2, PROBLEMA DE CONJUNTO DE DADOS NÃO GLOBULARES
+"""
+
+dados2 = pd.read_csv('kmeans_exemplo2.csv')
+print(dados2)
+
+sns.scatterplot(data=dados2, x='x', y='y')
+
+kmeans = KMeans(n_clusters=2,n_init=10,init='random',max_iter=300)
+kmeans.fit(dados2)
+
+dados2['cluster'] = kmeans.labels_
+sns.scatterplot(data=dados2, x='x', y='y', hue="cluster")
+
+print(kmeans.cluster_centers_)
+
+
+"""
+EXEMPLO 3, PROBLEMA DE CONJUNTO DE DADOS COM CLUSTERS COM DENSIDADES ALTAS
+"""
+
+dados3 = pd.read_csv('kmeans_exemplo3.csv')
+print(dados3)
+
+sns.scatterplot(data=dados3, x='x', y='y')
+
+kmeans = KMeans(n_clusters=4,n_init=10,init='random',max_iter=300)
+kmeans.fit(dados3)
+
+dados3['cluster'] = kmeans.labels_
+
+sns.scatterplot(data=dados3, x='x', y='y', hue='cluster')
+
+print(kmeans.cluster_centers_)
+
+"""
+Vamos tentar mitigar esse problema, aumentando o número de clusters!
+"""
+
+dados3 = pd.read_csv('kmeans_exemplo3.csv')
+
+kmeans = KMeans(n_clusters=16,n_init=10,init='random',max_iter=300)
+kmeans.fit(dados3)
+
+dados3['cluster'] = kmeans.labels_
+
+sns.scatterplot(data=dados3, x='x', y='y', hue="cluster", legend=None)
+
+"""
+Exemplo 4
+Conjunto de dados com sete tipos diferentes de grãos de feijão secos, levando em consideração as características como forma, formato, tipo e estrutura. Um sistema de visão computacional foi desenvolvido para distinguir sete diferentes variedades registradas de feijão seco com características semelhantes. Imagens de 13.611 grãos de 7 diferentes grãos secos registrados foram tiradas com uma câmera de alta resolução.
+
+Koklu, Murat, and Ilker Ali Ozkan. "Multiclass classification of dry beans using computer vision and machine learning techniques." Computers and Electronics in Agriculture 174 (2020): 105507.
+
+"""
+
+df = pd.read_csv('kmeans_exemplo4_drybean.csv')
+print(df)
+
+#Plotando um histograma da frequencia
+
+#argumento bins: Número de compartimentos do histograma a serem usados
+#argumento xrot: Rotação dos labels do eixo X, em graus
+df.Class.hist(bins=7,xrot=90)
+
+"""
+Vamos verificar se o k-means consegue capturar, 
+de forma não supervisionada, grupos de sementes com 
+características similares.
+"""
+
+"""
+Excluindo a coluna classe, que classifica. 
+"""
+dados4 = df.copy()
+del dados4['Class']
+
+kmeans = KMeans(n_clusters=7,n_init=10,init='random',max_iter=300)
+kmeans.fit(dados4)
+dados4['cluster'] = kmeans.labels_
+
+sns.scatterplot(data=dados4, x='Extent', y='Compactness', hue="cluster")
+
+"""Por padrão, esta função criará uma grade de eixos 
+de forma que cada variável numérica nos dados seja compartilhada 
+entre os eixos y em uma única linha e os eixos x em uma única coluna. 
+Os gráficos diagonais são tratados de forma diferente: um gráfico de 
+distribuição univariada é desenhado para mostrar a distribuição marginal
+ dos dados em cada coluna."""
+
+sns.pairplot(dados4,hue='cluster')
+
+df['cluster'] = kmeans.labels_
+"""
+Lendo os 30 primeiros
+"""
+df.head(30)
+
+"""
+Lendo os 30 ultimos
+"""
+df.tail(30)
--- Classificacao/knn.py
+++ Classificacao/knn.py
@@ -8,3 +8,135 @@ from sklearn.metrics import classification_report,plot_confusion_matrix,confusio
 from sklearn import preprocessing
 import numpy as np
 from sklearn.dummy import DummyClassifier
+
+
+
+
+"""
+Leitura da Base de Dados
+O banco de dados contém 14 atributos com base em testes físicos de um paciente. Amostras de sangue foram coletadas e o paciente também realizou um breve teste de esforço. O atributo "target" refere-se à presença de doença cardíaca no paciente (classe). É um número inteiro (0 para nenhuma presença, 1 para presença).
+
+Em geral, confirmar 100% se um paciente tem doença cardíaca pode ser um processo bastante invasivo, portanto, se pudermos criar um modelo que preveja com precisão a probabilidade de doença cardíaca, podemos ajudar a evitar procedimentos caros e invasivos.
+
+age
+sex
+chest pain type (4 values)
+resting blood pressure
+serum cholestoral in mg/dl
+fasting blood sugar > 120 mg/dl
+resting electrocardiographic results (values 0,1,2)
+maximum heart rate achieved
+exercise induced angina
+oldpeak = ST depression induced by exercise * relative to rest
+the slope of the peak exercise ST segment
+number of major vessels (0-3) colored by flourosopy
+thal: 3 = normal; 6 = fixed defect; 7 = reversable defect
+target:0 for no presence of heart disease, 1 for presence of heart disease
+
+Referência: Detrano, R., Janosi, A., Steinbrunn, W., Pfisterer, M., Schmid, J., Sandhu, S., Guppy, K., Lee, S., & Froelicher, V. (1989). International application of a new probability algorithm for the diagnosis of coronary artery disease. American Journal of Cardiology, 64,304--310.
+"""
+#Lendo o CSV
+df = pd.read_csv('heart.csv')
+df
+
+"""
+Analisando atributos
+Verificar relações simples entre atributos e a classe.
+"""
+#Criando o histograma
+df.target.hist() # verificando balanceamento da base de dados
+
+sns.pairplot(df[['age','trestbps','chol','oldpeak','target']], hue='target')
+plt.show()
+
+"""
+Classificação kNN
+Dividindo o conjunto de dados em treino (70%) e teste (30%).
+"""
+#random_state: Argumento para ramdomizar os conjuntos de treinamento e test
+df_train, df_test = train_test_split(df, test_size=0.3, random_state=42)
+
+df_train
+
+#Retorna as estatísticas do dataframe
+df_train.describe()
+
+df_test
+
+"""
+Preparando o classificador kNN
+
+k=7
+Distância Euclidiana
+Votação majoritária
+"""
+# estamos explicitamente selecionando os atributos de interesse
+knn = KNeighborsClassifier(n_neighbors=7)
+#Treinando o modelo com o dataframe de treinamento
+#X_train é os dados de treino
+X_train = df_train[['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal']]
+#y_train é os valores alvo dos dados de treino
+y_train = df_train[['target']]
+knn.fit(X_train, y_train.values.ravel())
+
+#X_test é os dados para testar o treinamento
+X_test = df_test[['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal']]
+#y_train é os valores alvo que o modelo deve atingir, que são usados para avaliação
+y_test = df_test[['target']]
+
+#Predição somente com os dados de teste, sem o target
+y_pred = knn.predict(X_test)
+
+#Avaliando em comparação do esperado
+print(classification_report(y_test, y_pred))
+
+
+# Classificador nulo que apenas chuta uma resposta considerando a distribuições das classes
+#ESTRATÉGIA PIOR
+dummy_clf = DummyClassifier(strategy='stratified')
+dummy_clf.fit(X_train,y_train)
+print(classification_report(y_test, dummy_clf.predict(X_test)))
+
+
+#USANDO VOTO PONDERADO
+knn_ponderado = KNeighborsClassifier(n_neighbors=7,weights='distance')
+X_train = df_train[['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal']]
+y_train = df_train[['target']]
+knn_ponderado.fit(X_train, y_train.values.ravel())
+
+y_pred = knn_ponderado.predict(X_test)
+
+print(classification_report(y_test, y_pred))
+
+"""
+Comparando com padronização de dados
+"""
+
+
+# preparando scaler para padronizar escala dos dados
+# usaremos o conjunto de treino
+min_max_scaler = preprocessing.MinMaxScaler()
+min_max_scaler.fit(df_train[['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal']])
+
+# transformar os dados para a nova escala (treino e teste)
+df_train_norm = min_max_scaler.transform(df_train[['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal']])
+df_test_norm = min_max_scaler.transform(df_test[['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal']])
+
+# preparando o knn com voto ponderado
+knn_norm = KNeighborsClassifier(n_neighbors=7,weights='distance')
+
+# conjunto de treino
+X_train = df_train_norm
+y_train = df_train[['target']]
+knn_norm.fit(X_train, y_train.values.ravel())
+
+# conjunto de teste
+X_test = df_test_norm
+y_test = df_test[['target']]
+
+y_pred = knn_norm.predict(X_test)
+
+# comparando desempenho
+print(classification_report(y_test, y_pred))
+
+pd.DataFrame(df_train_norm).describe()
